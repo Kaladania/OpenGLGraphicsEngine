283ad62291a9fc9ec9781b74a73a80cdeac9da6c
Kaladania <72551649+Kaladania@users.noreply.github.com>
2024-03-26 15:54:44 +0000

    Rotation Upgrade
    Triangle now rotates automatically
    Squares can rotate forwards and backwards via keypresses

---
+++ Tutorial5/Tutorial2/GLUTCallbacks.cpp
@@ -0,0 +1,36 @@
+#include "GLUTCallbacks.h"
+#include "HelloGL.h"
+
+namespace GLUTCallbacks //creates free methods wrappers to allow FreeGLUT to access class methods
+{
+	namespace // annonymous namespace restricts write access to point to only code within namespace
+	{
+		HelloGL* helloGL = nullptr;
+	}
+
+	void Init(HelloGL* gl) //sets the pointer to the passed in class instance
+	{
+		helloGL = gl;
+	}
+
+	void Display() //calls the instance's display setup function
+	{
+		if (helloGL != nullptr)
+		{
+			helloGL->Display();
+		}
+	}
+
+	void GameTimer(int prefferedRefresh) 
+	{
+		int updateTime = glutGet(GLUT_ELAPSED_TIME); //gets the amount of time it takes to start the timer/scene
+		helloGL->Update();
+		updateTime -= glutGet(GLUT_ELAPSED_TIME);
+		glutTimerFunc(prefferedRefresh - updateTime, GLUTCallbacks::GameTimer, prefferedRefresh); //sets the frame rate offset by the start-up time
+	}
+
+	void Keyboard(unsigned char key, int x, int y)
+	{
+		helloGL->Keyboard(key, x, y);
+	}
+}
